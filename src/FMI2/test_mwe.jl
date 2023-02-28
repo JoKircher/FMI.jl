a3bab534e59976b46373f573a54227272b792cae
JoKircher <josef_kircher@gmx.de>
2023-02-28 14:26:01 +0100

    more things to be tested

--- src/FMI2/test_mwe.jl
+++ src/FMI2/test_mwe.jl
@@ -1,13 +1,23 @@
-using FMI, FMIImport
+using FMI, FMIImport, FMICore
 
 myFMU = fmiLoad(string(pwd(), "/src/FMI2/Mwe.fmu"))
 fmiInfo(myFMU)
-fmi2SetContinuousStates(myFMU, [0.1])
-myFMU.components[end].state
+
 fmiInstantiate!(myFMU)
 fmi2SetupExperiment(myFMU)
 fmi2EnterInitializationMode(myFMU)
 fmi2ExitInitializationMode(myFMU)
+myFMU.components[end].state
+fmi2EnterContinuousTimeMode(myFMU)
+fmi2EnterEventMode(myFMU)
+fmi2NewDiscreteStates!(myFMU.components[end], myFMU.components[end].eventInfo)
+out = zeros(2)
+fmi2CompletedIntegratorStep(myFMU, FMICore.fmi2True)
+fmi2GetEventIndicators!(myFMU.components[end], out)
+fmi2SetContinuousStates(myFMU, [0.1])
+FMIImport.fmi2GetNominalsOfContinuousStates(myFMU)
+fmi2GetContinuousStates(myFMU)
+myFMU.components[end].state
 # fmiIsModelExchange(myFMU)
 # fmi2EnterEventMode(myFMU)
 # fmi2EnterContinuousTimeMode(myFMU)
